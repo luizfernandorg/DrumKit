1f8519dafdc075aa34f199cea1146c17ee266b40
Luiz Fernando <luizfernandorg@gmail.com>
2023-06-26 15:29:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 const drums = document.querySelectorAll(".drum")
 const audio = new Audio()
-
-function playSound(instrument){
-    if(instrument === 'w'){
-        audio.src = 'sounds\\tom-1.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else if(instrument === 'a'){
-        audio.src = 'sounds\\tom-2.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else if(instrument === 's'){
-        audio.src = 'sounds\\tom-3.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else if(instrument === 'd'){
-        audio.src = 'sounds\\tom-4.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else if(instrument === 'j'){
-        audio.src = 'sounds\\snare.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else if(instrument === 'k'){
-        audio.src = 'sounds\\crash.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else if(instrument === 'l'){
-        audio.src = 'sounds\\kick-bass.mp3'
-        audio.play()
-        buttonAnimation(instrument)
-    } else {
-        console.log("This is not the right instrument!")
-    }
-}
+const setInstrumment = (instrument, sound) => {
+		audio.src = sound
+		audio.play()
+		buttonAnimation(instrument)
+	}
+	
+	function playSound(instrument){
+		if(instrument === 'w'){
+			setInstrumment(instrument, 'sounds\\tom-1.mp3')
+		} else if(instrument === 'a'){
+			setInstrumment(instrument, 'sounds\\tom-2.mp3')
+		} else if(instrument === 's'){
+			setInstrumment(instrument, 'sounds\\tom-3.mp3')
+		} else if(instrument === 'd'){
+			setInstrumment(instrument, 'sounds\\tom-4.mp3')
+		} else if(instrument === 'j'){
+			setInstrumment(instrument, 'sounds\\snare.mp3')
+		} else if(instrument === 'k'){
+			setInstrumment(instrument, 'sounds\\crash.mp3')
+		} else if(instrument === 'l'){
+			setInstrumment(instrument, 'sounds\\kick-bass.mp3')
+		} else {
+			console.log("This is not the right instrument!")
+		}
+	}
 
 drums.forEach((v,index) => {
     v.addEventListener("click", (e) => {
